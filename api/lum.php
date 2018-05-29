4ca38fcf4fbefb2d02faecc16952b93372f2a5dd
Tony Duane Smith <tonyduanesmith@me.com>
2018-05-29 15:23:49 +0100

    Added luminance script

---
+++ api/lum.php
@@ -0,0 +1,22 @@
+<?php
+require('config.php');
+require('error.php');
+
+$connection = mysqli_connect($host,$username,$password,$database);
+$sql = "SELECT * FROM colourLookup";
+$result = mysqli_query($connection, $sql);
+foreach ($result as $key => $row) {
+    $lum = luminance($row['red'], $row['green'], $row['blue']);
+    $id = $row['ID'];
+    $sqlUpdate = "UPDATE colourLookup SET luminance=$lum WHERE ID LIKE $id";
+    $resultUpdate = mysqli_query($connection, $sqlUpdate);
+}
+
+
+
+function luminance($red, $green, $blue){
+    return (0.2126*$red) + (0.7152*$green) + (0.0722*$blue);
+}
+
+
+?>
